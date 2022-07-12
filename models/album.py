736d9fcee54983cce05735e55c0428be96214e0c
Naefunn <nathan.d.foster1@gmail.com>
2022-07-12 14:45:03 +0100

    classes set up

---
+++ models/album.py
@@ -0,0 +1,7 @@
+class Album:
+    def __init__(self, _title, _genre, _artist, _id = None, ):
+        self.title = _title
+        self.genre = _genre
+        self.artist = _artist
+        self.id = _id
+        
